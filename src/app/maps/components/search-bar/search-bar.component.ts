47b9febc6ddef1c6427cd20e1475e18f54221f27
EdsonJSF <edsonjsf1990@gmail.com>
2023-05-18 17:01:58 -0400

    Ocultar el menu de lugares

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
 
+import { Feature } from '../../interfaces/places.interface';
+
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styles: [],
 })
 export class SearchBarComponent {
+  show: boolean = true;
+  get places(): Feature[] {
+    return this.placesService.places;
+  }
+
   private debounceTimer?: NodeJS.Timeout;
 
   constructor(private placesService: PlacesService) {}
@@ -15,6 +22,7 @@ export class SearchBarComponent {
     if (this.debounceTimer) clearTimeout(this.debounceTimer);
 
     this.debounceTimer = setTimeout(() => {
+      this.show = true;
       this.placesService.getPlacesByQuery(query);
     }, 350);
   }
